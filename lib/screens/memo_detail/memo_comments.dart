d557063d78e3bbfba6cd347a9cc0d87bdfbf207a
erauner <erauner@medallia.com>
2025-04-09 09:44:58 -0500

    wip

--- lib/screens/memo_detail/memo_comments.dart
+++ lib/screens/memo_detail/memo_comments.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_memos/providers/comment_providers.dart';
 import 'package:flutter_memos/providers/ui_providers.dart';
 import 'package:flutter_memos/screens/memo_detail/memo_detail_providers.dart';
+import 'package:flutter_memos/utils/comment_utils.dart'; // Add import for comment sorting utilities
 import 'package:flutter_memos/widgets/comment_card.dart'; // Import the widget directly
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -24,6 +25,9 @@ class MemoComments extends ConsumerWidget {
             comments
                 .where((c) => !hiddenCommentIds.contains('$memoId/${c.id}'))
                 .toList();
+
+        // Sort comments to show pinned first, then oldest to newest for chronological reading
+        CommentUtils.sortByPinnedThenOldestFirst(visibleComments);
 
         if (visibleComments.isEmpty) {
           return Padding(
